3c9e0b9a6e2339196978fcd2dfff863e7f27e841
daryapalevitch <135000068+daryapalevitch@users.noreply.github.com>
2023-05-30 09:40:57 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220);
+ 
+if(key=='g') {
+  
+ 
+   //צב 1
+  stroke("#4CAF50")
+strokeWeight(6)
+beginShape();
+vertex(139.66,377.8);
+bezierVertex(139,377,227,153,371,189);
+bezierVertex(386,193,396,205,397,220);
+bezierVertex(399,250,378,302,249,365);
+bezierVertex(47,462,138,526,173,514);
+bezierVertex(205,504,225,450,225,450);
+endShape()
+fill("rgba(0, 0, 0, 0)")
+stroke("#4CAF50")
+strokeWeight(6)
+beginShape();
+vertex(415,268);
+bezierVertex(415,268,430,224,428,191);
+bezierVertex(425,161,430,91,488,109);
+bezierVertex(550,128,482,243,433,294);
+bezierVertex(390,339,354,378,353,415);
+bezierVertex(353,427,361,438,373,439);
+bezierVertex(383,441,394,436,398,413);
+bezierVertex(407,366,421,351,421,351);
+endShape()
+}
+ 
+  
+ 
+if(key=='h'){
+   //צב שתיים
+  
+stroke("#4CAF50")
+strokeWeight(6)
+beginShape();
+vertex(129.31,336.23);
+bezierVertex(129,336,264,136,397,204);
+bezierVertex(410,211,418,226,415,240);
+bezierVertex(410,270,379,316,238,348);
+bezierVertex(20,398,95,480,131,477);
+bezierVertex(165,474,197,426,197,426);
+endShape()
+fill("rgba(0, 0, 0, 0)")
+stroke("#4CAF50")
+strokeWeight(6)
+beginShape();
+vertex(422.9,291.51);
+bezierVertex(422,291,447,251,452,218);
+bezierVertex(456,189,476,122,529,152);
+bezierVertex(585,184,493,281,434,320);
+bezierVertex(382,354,338,385,329,420);
+bezierVertex(326,432,332,444,343,448);
+bezierVertex(352,452,364,450,373,428);
+bezierVertex(392,385,409,373,409,373);
+endShape() 
+}
+
+
+  
+  
+  //גלים
+stroke("#5F8ED6")
+strokeWeight(6)
+beginShape();
+vertex(33,88);
+bezierVertex(33,88,97,44,130,70);
+bezierVertex(162,97,157,116,199,110);
+endShape()
+fill("rgba(0, 0, 0, 0)")
+stroke("#1D64B0")
+strokeWeight(6)
+beginShape();
+vertex(364.61,499.07);
+bezierVertex(364,499,432,460,462,489);
+bezierVertex(493,518,486,537,528,534);
+endShape()
+
+
+}
